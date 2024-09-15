86fccd473919538974a8d119a85f9ff006fe1406
CaliforniaX <127232270+CaliforniaX@users.noreply.github.com>
2024-09-15 10:02:44 +0100

    Add files via upload

---
+++ scripts/default-programs.ps1
@@ -0,0 +1,8 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+cmd.exe /c "winget install Alex313031.Thorium.AVX2"
+cmd.exe /c "winget install Valve.Steam"
+cmd.exe /c "winget install Guru3D.Afterburner"
+cmd.exe /c "winget install Discord.Discord"
+cmd.exe /c "winget install Modrinth.ModrinthApp"
+cmd.exe /c "winget install VideoLAN.VLC"
+cmd.exe /c "winget install 7zip.7zip"
